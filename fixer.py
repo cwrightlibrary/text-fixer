fd1c36f1bd503f749d199dd942e8b16dea38a0ee
cwrightlibrary <108359193+cwrightlibrary@users.noreply.github.com>
2024-03-07 17:31:21 -0500

    Add files via upload

--- fixer.py
+++ fixer.py
@@ -2,18 +2,20 @@ from docx import Document
 from docx.shared import Pt
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from os.path import dirname, join, realpath
-from spellchecker import SpellChecker
 import re
 
 current_dir = dirname(realpath(__file__))
-spell = SpellChecker()
 
 
 def get_job_info(doc):
     original_doc = Document(join(current_dir, doc))
 
     for paragraph in original_doc.paragraphs:
-        if paragraph.text[0:11] == "Job Title: ":
+        if "submission date" in paragraph.text.lower():
+            job_date = paragraph.text[17:]
+        if paragraph.text[0:17] == "Submission Date: ":
+            job_date = paragraph.text[17:]
+        elif paragraph.text[0:11] == "Job Title: ":
             job_title = paragraph.text[11:]
         elif paragraph.text[0:17] == "Job Description: ":
             job_desc = paragraph.text[17:]
@@ -29,6 +31,7 @@ def get_job_info(doc):
             job_contact = paragraph.text[9:]
 
     job = {
+        "date": job_date,
         "title": job_title,
         "desc": job_desc,
         "qual": job_qual,
@@ -41,13 +44,18 @@ def get_job_info(doc):
     return job
 
 
-original_doc = get_job_info("job-list.docx")
+def fix_date(element):
+    new_element = element
+    
+    return new_element
+
 
 def fix_title(element):
     new_element = element
     new_element = new_element.replace("and", "&").title()
     
     return new_element
+
 
 def fix_description(element):
     new_element = element
@@ -67,10 +75,9 @@ def fix_description(element):
 
     if new_element[len(new_element) - 1:] != ".":
         new_element += "."
-
-    new_element = new_element.replace("Caroliniana", "Carolina").replace("caroliniana", "carolina")
     
     return new_element
+
 
 def fix_qualifications(element):
     new_element = element
@@ -93,6 +100,7 @@ def fix_qualifications(element):
     
     return new_element
 
+
 def fix_type(element):
     new_element = element
     if "part" in new_element or "Part" in new_element:
@@ -104,9 +112,74 @@ def fix_type(element):
     
     return new_element
 
-def fix_salary(element):
-    pass
 
-# test_file = open(join(current_dir, "test_file.txt"), "w", encoding="utf-8")
-# test_file.write(testing)
-# test_file.close()
+def fix_salary(element):
+    new_element = element
+    if new_element == "":
+        new_element = ""
+    
+    return new_element
+
+
+def fix_contact(element):
+    new_element = element
+    letter_spacing = re.findall("[a-z][A-Z]", new_element)
+    
+    for e in letter_spacing:
+        if e[0] != "h" and e[1] != "D":
+            new_element = new_element.replace(e, e[0] + " " + e[1])
+    
+    phone = re.compile(r'\b(?:\d{3}[-.\s]?)?\d{3}[-.\s]?\d{4}\b').search(new_element)
+    
+    if phone:
+        phone = phone.group()
+        new_element = new_element.replace(" " + phone, ", " + phone)
+    else:
+        phone = ""
+    
+    return new_element
+
+
+def load_fix_document(loc):
+    original_doc = get_job_info(loc)
+
+    new_doc = {}
+    new_doc["Submission Date: "] = fix_date(original_doc["date"])
+    new_doc["Job Title: "] = fix_title(original_doc["title"])
+    new_doc["Job Description: "] = fix_description(original_doc["desc"])
+    new_doc["Qualifications: "] = fix_qualifications(original_doc["qual"])
+    new_doc["Type: "] = fix_type(original_doc["type"])
+    new_doc["How to Apply: "] = original_doc["how"]
+    new_doc["Salary: "] = fix_salary(original_doc["salary"])
+    new_doc["Contact: "] = fix_contact(original_doc["contact"])
+    
+    return new_doc
+
+
+def create_new_document(new_doc):
+    new_document = Document()
+    style = new_document.styles["Normal"]
+    font = style.font
+    font.name = "Arial"
+    hstyle = new_document.styles["Heading 2"]
+    hstyle.font.name = "Arial"
+    hstyle.font.size = Pt(16)
+    
+    h = new_document.add_heading("", 2)
+    h.add_run(new_doc["Job Title: "] + ", " + new_doc["Submission Date: "]).bold = False
+    s = new_document.add_paragraph()
+    
+    for k, v in new_doc.items():
+        p = new_document.add_paragraph()
+        p.style = new_document.styles["Normal"]
+        p.add_run(k).bold = True
+        p.add_run(v)
+        p.add_run()
+    
+    return new_document
+
+
+fix_document = load_fix_document("job-list.docx")
+new_document = create_new_document(fix_document)
+
+new_document.save(join(current_dir, "test.docx"))
